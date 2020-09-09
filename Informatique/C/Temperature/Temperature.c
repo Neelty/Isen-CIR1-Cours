fda7288664b1f10f5099b57570b4364dd02709e0
Pim <SimonHauguel>
2020-09-09 19:42:58 +0200

    09/09/2020

--- Informatique/C/Temperature/Temperature.c
+++ Informatique/C/Temperature/Temperature.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 /*  Premier Programme, fait le 07/09/2020 */
 
@@ -8,8 +9,8 @@ int main(int argc, char *argv[]) {
   double tf; /*  Definit tf en tant qu'un double */
   printf("Tempéreature fahrenheit\n"); /*  Simple print\n  */
   scanf("%lf", &tf); /* Get l'entrée clavier de l'utilisateur, et place la valeur 'dans' tf (type de l'entrée castée par le '%lf') */
-  printf("%lf\n", (tf+459.67)*(5.0/9));/* Print notre valeur obtenue, après notre transformation */
+  printf("%lf\n", (tf+459.67)*(5.0/9)); /* Print notre valeur obtenue, après notre transformation */
 
-  return 0; /*  On marque la fin de notre programme */
+  return EXIT_SUCCESS; /*  On marque la fin de notre programme */
 
 }
