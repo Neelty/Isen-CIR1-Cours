794cb41fe03aaebb3fe053448b4cd198a70a528d
Rémi Van Boxem <39855540+Neelty@users.noreply.github.com>
2020-09-11 22:38:34 +0200

    Quelques corrections sur le projet température
    Voici quelques modifications apportées sur le fichier temperature.c : 
    
    Définition de la langue ainsi que les caractères à utiliser dans le terminal,
    Ajout de quelques précisions dans les commentaires.

--- Informatique/C/Temperature/Temperature.c
+++ Informatique/C/Temperature/Temperature.c
@@ -5,10 +5,12 @@
 /*  Premier Programme, fait le 07/09/2020 */
 
 int main(int argc, char *argv[]) {
-
-  double tf; /*  Definit tf en tant qu'un double */
-  printf("Tempéreature fahrenheit\n"); /*  Simple print\n  */
-  scanf("%lf", &tf); /* Get l'entrée clavier de l'utilisateur, et place la valeur 'dans' tf (type de l'entrée castée par le '%lf') */
+  
+  setlocale(LC_ALL, "fr-FR"); /*  Définit la langue ainsi que les caractères à utiliser dans le temrinal */
+  
+  double tf; /*  Definit tf en tant qu'une variable double */
+  printf("Température en Fahrenheit\n"); /*  Simple print\n  */
+  scanf("%lf", &tf); /* Récupère l'entrée clavier de l'utilisateur, et place la valeur 'dans' tf (type de l'entrée castée par le '%lf') */
   printf("%lf\n", (tf+459.67)*(5.0/9)); /* Print notre valeur obtenue, après notre transformation */
 
   return EXIT_SUCCESS; /*  On marque la fin de notre programme */
